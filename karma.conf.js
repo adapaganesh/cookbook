d4406a88766ebc3cbeb987a8043725e0edc0601a
Ganapathi Adapa <gadapa@gmaccm.com>
2016-12-15 18:48:32 +0530

    Initial Setup

--- karma.conf.js
+++ karma.conf.js
@@ -2,23 +2,33 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
+  var host = process.env.HOST_IP;
+  var webdriverConfig = {
+    hostname: host,
+    port: 8910
+  };
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', 'angular-cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-webdriver-launcher'),
       require('karma-remap-istanbul'),
       require('angular-cli/plugins/karma')
     ],
+    customLaunchers: {
+      phantomjs_webdriver: {
+        browserName: 'PhantomJS',
+        base: 'WebDriver',
+        config: webdriverConfig
+      }
+    },
     files: [
       { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
       './src/test.ts': ['angular-cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
     },
     remapIstanbulReporter: {
       reports: {
@@ -31,13 +41,14 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'karma-remap-istanbul']
-              : ['progress'],
-    port: 9876,
+        ? ['progress', 'karma-remap-istanbul']
+        : ['progress'],
+    hostname: host,
+    port: process.env.KARMA_PORT,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['phantomjs_webdriver'],
     singleRun: false
   });
 };
